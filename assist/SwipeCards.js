430a00e51df002d28e9a8b220cb219544818abad
Mayank Musaddi <mayank.musaddi@research.iiit.ac.in>
2020-11-08 10:49:56 +0530

    last

--- assist/SwipeCards.js
+++ assist/SwipeCards.js
@@ -1,10 +1,10 @@
 // SwipeCards.js
-'use strict';
+"use strict";
 
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
-import SwipeCards from 'react-native-swipe-cards';
+import SwipeCards from "react-native-swipe-cards";
 
 class Card extends React.Component {
   constructor(props) {
@@ -13,10 +13,12 @@ class Card extends React.Component {
 
   render() {
     return (
-      <View style={[styles.card, { backgroundColor: this.props.backgroundColor }]}>
+      <View
+        style={[styles.card, { backgroundColor: this.props.backgroundColor }]}
+      >
         <Text>{this.props.text}</Text>
       </View>
-    )
+    );
   }
 }
 
@@ -28,45 +30,60 @@ class NoMoreCards extends Component {
   render() {
     return (
       <View>
-        <Text style={styles.noMoreCardsText}>Great! You have finished all the Questions</Text>
+        <Text style={styles.noMoreCardsText}>
+          Great! You have finished all the Questions
+        </Text>
       </View>
-    )
+    );
   }
 }
 
 const cards = [
-  { text: '1', backgroundColor: 'cyan' },
-  { text: '2', backgroundColor: 'cyan' },
-  { text: '3', backgroundColor: 'cyan' },
-  { text: '4', backgroundColor: 'cyan' },
-  { text: '5', backgroundColor: 'cyan' },
-  { text: '6', backgroundColor: 'cyan' },
-  { text: '7', backgroundColor: 'cyan' },
-  { text: '8', backgroundColor: 'cyan' },
-  { text: '9', backgroundColor: 'cyan' },
-]
+  { text: "What is a chronotype?", backgroundColor: "cyan" },
+  {
+    text:
+      "What are the two extremes with most individuals having some flexibility in the timing of their sleep period?",
+    backgroundColor: "cyan",
+  },
+  { text: "what is a notable environmental cue?", backgroundColor: "cyan" },
+  {
+    text:
+      "What is a factor that might be important in determining a person's chronotype?",
+    backgroundColor: "cyan",
+  },
+  { text: "What are the circadian rhythms?", backgroundColor: "cyan" },
+  {
+    text: "What is the main thing that humans do during the day?",
+    backgroundColor: "cyan",
+  },
+  { text: "What are clock genes called?", backgroundColor: "cyan" },
+];
+
+const testText =
+  "A chronotype is the behavioral manifestation of underlying circadian rhythms of myriad physical processes. A person's chronotype is the propensity for the individual to sleep at a particular time during a 24-hour period. Eveningness (delayed sleep period) and morningness (advanced sleep period) are the two extremes with most individuals having some flexibility in the timing of their sleep period. However, across development there are changes in the propensity of the sleep period with pre-pubescent children preferring an advanced sleep period, adolescents preferring a delayed sleep period and many elderly preferring an advanced sleep period.\nThe causes and regulation of chronotypes, including developmental change, individual propensity for a specific chronotype, and flexible versus fixed chronotypes have yet to be determined. However, research is beginning to shed light on these questions, such as the relationship between age and chronotype.[1] There are candidate genes (called clock genes) that exist in most cells in the body and brain, referred to as the circadian system that regulate physiological phenomena (hormone levels, metabolic function, body temperature, cognitive faculties, and sleeping). With the exception of the most extreme and rigid chronotypes, regulation is likely due to gene-environment interactions. Important environmental cues (zeitgebers) include light, feeding, social behavior, and work and school schedules. Additional research has proposed an evolutionary link between chronotype and nighttime vigilance in ancestral societies.[2]\nHumans are normally diurnal creatures, that is to say they are active in the daytime. As with most other diurnal animals, human activity-rest patterns are endogenously controlled by biological clocks with a circadian (~24-hour) period. Chronotypes have also been investigated in other species, such as fruit flies[3] and mice.[4] ";
 
 export default class extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       cards: cards,
-      score: "0"
+      score: 0,
     };
   }
 
   handleYup(card) {
-    this.state.score += 1
-    console.log(`${this.state.score}`)
-    console.log(`Yup for ${card.text}`)
+    this.setState({ score: this.state.score + 1 });
+    // this.state.score += 1;
+    console.log(`${this.state.score}`);
+    console.log(`Yup for ${card.text}`);
   }
   handleNope(card) {
-    this.state.score -= 1
-    console.log(`Nope for ${card.text}`)
+    this.setState({ score: this.state.score - 1 });
+    // this.state.score -= 1;
+    console.log(`Nope for ${card.text}`);
   }
   handleMaybe(card) {
-    console.log(`Maybe for ${card.text}`)
+    console.log(`Maybe for ${card.text}`);
   }
   render() {
     return (
@@ -82,7 +99,7 @@ export default class extends React.Component {
         />
         <Text style={styles.score}>SCORE IS {this.state.score}</Text>
       </View>
-    )
+    );
   }
 }
 
@@ -90,23 +107,26 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: 23,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   card: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: 30,
+    padding: 30,
     width: 300,
     height: 300,
   },
   noMoreCardsText: {
     fontSize: 22,
-    alignItems: 'center',
-    textAlign: 'center'
+    alignItems: "center",
+    textAlign: "center",
   },
-  score:{
+  score: {
+    fontSize: 20,
     paddingTop: 10,
     marginTop: 10,
-  }
-})
+  },
+});
--- assist/input.js
+++ assist/input.js
@@ -1,66 +1,158 @@
-import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
+import React, { Component } from "react";
+import SERVER_URL from "./url.js";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+} from "react-native";
 
 class Inputs extends Component {
-    state = {
-        text: ''
-    }
-    handleText = (text) => {
-        this.setState({ text: text })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
 
-    login = (text) => {
-        this.props.navigation.navigate('Questions')
-    }
-    render() {
-        return (
-            <View style={styles.container}>
-                <TextInput style={styles.input}
-                    underlineColorAndroid="transparent"
-                    placeholder="Paste your content here!"
-                    placeholderTextColor="#9a73ef"
-                    autoCapitalize="none"
-                    onChangeText={this.handleText} />
+      squashId: null,
+      settings: {
+        top_p: 0.9,
+        gen_frac: 0.5,
+        spec_frac: 0.5,
+      },
+      ans_mode: "original",
+      forest: null,
+      queue_number: null,
+      input_text: null,
+      status: null,
+      expanded: null,
+      dropdownOpen: false,
+    };
+  }
 
-                <TouchableOpacity
-                    style={styles.submitButton}
-                    onPress={
-                        () => this.login(this.state.text)
-                    }>
-                    <Text style={styles.submitButtonText}> Submit </Text>
-                </TouchableOpacity>
-            </View>
-        )
+  handleText = (text) => {
+    this.setState({ text: text });
+  };
+
+  getSquashedDocument() {
+    if (this.state.squashId) {
+      var url = SERVER_URL + "/get_squash_doc?id=" + this.state.squashId;
+      console.log(this.state.squashId);
+
+      fetch(url)
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            console.log(result);
+            // if (result.input_text) {
+            //   document.getElementById("squashInputText").value =
+            //     result.input_text;
+            // }
+            // var expanded = null;
+            // if (result.squash_data != null) {
+            //   expanded = result.squash_data.qa_tree.map((para, para_index) =>
+            //     para.binned_qas.map((qa_tree, qa_index) => false)
+            //   );
+            // }
+            // this.setState({
+            //   forest: result.squash_data,
+            //   queue_number: result.queue_number,
+            //   settings: {
+            //     top_p: result.settings.top_p,
+            //     gen_frac: result.settings.gen_frac,
+            //     spec_frac: result.settings.spec_frac,
+            //   },
+            //   status: result.status,
+            //   expanded: expanded,
+            // });
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
     }
+  }
+
+  squashDoc(text) {
+    var url = SERVER_URL + "/request_squash_doc";
+    var flags = {
+      method: "POST",
+      body: JSON.stringify({
+        settings: this.state.settings,
+        input_text: text,
+      }),
+    };
+    fetch(url, flags)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          //   window.location.href = "/?id=" + result.new_id;
+          console.log(result.new_id);
+          this.setState({ squashId: result.new_id });
+          this.getSquashedDocument.bind(this)();
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  submit = (text) => {
+    // this.squashDoc(text);
+    this.props.navigation.navigate("Questions");
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder="Paste your content here!"
+          placeholderTextColor="#9a73ef"
+          autoCapitalize="none"
+          multiline={true}
+          onChangeText={this.handleText}
+        />
+
+        <TouchableOpacity
+          style={styles.submitButton}
+          onPress={() => this.submit(this.state.text)}
+        >
+          <Text style={styles.submitButtonText}> Submit </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
-export default Inputs
+export default Inputs;
 
 const styles = StyleSheet.create({
-    container: {
-        paddingTop: 23,
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    input: {
-        textAlign: "center",
-        margin: 15,
-        height: 300,
-        width: 300,
-        borderColor: '#7a42f4',
-        borderWidth: 1
-    },
-    submitButton: {
-        textAlign: "center",
-        backgroundColor: '#7a42f4',
-        padding: 10,
-        margin: 15,
-        height: 40,
-        alignSelf: "center",
-        width: 80
-    },
-    submitButtonText: {
-        color: 'white'
-    }
-})
+  container: {
+    paddingTop: 23,
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    textAlign: "center",
+    padding: 20,
+    margin: 15,
+    height: 300,
+    width: 300,
+    borderColor: "#7a42f4",
+    borderWidth: 1,
+  },
+  submitButton: {
+    textAlign: "center",
+    backgroundColor: "#7a42f4",
+    padding: 10,
+    margin: 15,
+    height: 40,
+    alignSelf: "center",
+    width: 80,
+  },
+  submitButtonText: {
+    color: "white",
+  },
+});
